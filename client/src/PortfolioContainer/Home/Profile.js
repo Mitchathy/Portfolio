1ba77fe35ae5707bd16e765ddbcb18b87eefabdc
Mitchell Walker <eagleye1144@gmail.com>
2022-07-26 19:33:43 -0600

    Added Resume Downlad

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -7,14 +7,23 @@ export default function Profile() {
                 <div className='profile-details'>
                     <div className='colz'>
                         <a href='https://github.com/Mitchathy'>
-                            <i className='fa fa-github'>
-                            </i>
+                            <i className='fa fa-github'> </i>
                         </a>
                         <a href='https://www.linkedin.com/in/mitchell-walker-b66698104/'>
                             <i className='fa fa-linkedin'>
                             </i>
                         </a>
                     </div>
+                    <div className="profile-details-name">
+                        <span className='primary-text'>
+                            Hello, I'm <span className='highlighted-text'>Mitch</span>
+                        </span>
+                    </div>
+                        <a href='Mitchell-Walker-resume(1).pdf' download='Mitchell Walker resume.pdf'>
+                            <button>
+                                 Download Resume
+                            </button>
+                        </a>
                 </div>
             </div>
 
