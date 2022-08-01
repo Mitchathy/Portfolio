71596f3f97cd64c44716372674fac442fa70fe67
Mitchell Walker <eagleye1144@gmail.com>
2022-08-01 14:56:48 -0600

    Added Project.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Profile from './PortfolioContainer/Home/Profile';
+import Projects from './PortfolioContainer/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Profile/>
+      <Projects/>
     </div>
   );
 }
---
+++ client/src/PortfolioContainer/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+       <div className='ProjectsContainer'>
+            <div className='Title'>
+                <h3>Projects</h3>
+            </div>
+       </div> 
+    )
+}
