412bd683144ab589ff40d26aec1d0b1ee00770ff
Mitchell Walker <eagleye1144@gmail.com>
2022-08-09 15:57:10 -0600

    Made Footer div

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Profile from './PortfolioContainer/Home/Profile';
 import Projects from './PortfolioContainer/Projects/Projects';
+import Footer from './PortfolioContainer/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Profile/>
       <Projects/>
+      <Footer/>
     </div>
   );
 }
