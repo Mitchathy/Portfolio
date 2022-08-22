00c50f60e0ad3ff191e778340461ce8ff3a73769
Mitchell Walker <eagleye1144@gmail.com>
2022-08-21 20:04:05 -0600

    Fixed Flexbox

--- client/src/PortfolioContainer/Projects/Projects.js
+++ client/src/PortfolioContainer/Projects/Projects.js
@@ -8,18 +8,17 @@ export default function Projects() {
                 <h3>Projects</h3>
             </div>
             <div className='Projects'>
-                <ul>
-                    <li>Nasa photo of the day</li>
+                    <div className='project1'>Nasa photo of the day
                         <a href='https://github.com/Mitchathy/nasa-photo-of-the-day'>github</a>
                             <a href='https://nasa-photo-of-the-3mmm4jozo-eagleye1144.vercel.app/'>App</a>
-                    <li>Water my Plants</li>
-                        <a href='Water_github'>github</a>
-                            <a href='Water_live'>App</a>
-                    <li>Secret Family cookbook</li>
+                                </div>
+                    <div className='project2'>Water my Plants
+                        <a href='https://github.com/Bulid-Week-Water-My-Plants/back-end'>github</a>
+                            </div>
+                    <div className='project3'>Secret Family recipes
                         <a href='Cookbook_github'>github</a>
-                            <a href='Cookbook_live'>App</a>
-                </ul>
-
+                            <a href='https://secret-recipes-gold.vercel.app/'>App</a>
+                                </div>
             </div>
        </div> 
     )
