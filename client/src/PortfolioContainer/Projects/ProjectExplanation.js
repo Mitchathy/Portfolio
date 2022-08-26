d0056d92258479c57f98ea7c243033e6791d3452
Mitchell Walker <eagleye1144@gmail.com>
2022-08-25 18:14:44 -0600

    Made new component. Trying to figure out state

---
+++ client/src/PortfolioContainer/Projects/ProjectExplanation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState } from "react";
+
+export default function ProjectExplanation(props) {
+    const {project} = props
+    
+    const [showText, setShowText] = useState(false)   
+
+        const toggleText = () =>{
+
+            setShowText(!showText)
+        }
+
+    return (
+        <div className= "ProjectInfo">
+            {showText && <div>{project}</div>}
+            <button onClick={toggleText}>
+                {showText ? 'Show Explanation' : 'Hide Explanation'}
+                </button>
+        </div>
+    )
+
+
+
+}
--- client/src/PortfolioContainer/Projects/Projects.js
+++ client/src/PortfolioContainer/Projects/Projects.js
@@ -2,15 +2,11 @@ import React from 'react'
 import './Projects.css'
 import explanationData from './ExplanationData'
 import { useState } from 'react'
+import ProjectExplanation from './ProjectExplanation'
 
 export default function Projects() {
     console.log(explanationData[0])
-    const ShowExplanation = (explanationData) => {
-        
-        const [showText, setShowText] = useState(false)
 
-   
-    }
 
     return (
        <div className='ProjectsContainer'>
@@ -23,7 +19,7 @@ export default function Projects() {
                             <i className='fa fa-github'> </i>
                                 </a>
                             <a href='https://nasa-photo-of-the-3mmm4jozo-eagleye1144.vercel.app/'>App</a>
-                                <button className='Nasa' onClick = {explanationData[0]}>Show Explanation</button>
+                                <button className='Nasa'>Show Explanation</button>
                                  </div>
                     <div className='project2'>Water my Plants
                         <a href='https://github.com/Bulid-Week-Water-My-Plants/back-end'>
